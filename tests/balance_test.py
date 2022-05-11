4cd3bd105abc5e4650d10118e5f3212a05bf89bb
SatyaAbhilash98 <abhilash.dommeti@gmail.com>
2022-05-11 17:12:28 -0400

    fixed balance

--- tests/balance_test.py
+++ tests/balance_test.py
@@ -1,42 +1,41 @@
+import csv
+
+from flask import url_for
+from flask_login import current_user
+
 from app import db
 from app.db.models import User, Transaction
+def test_balance_calculation(client):
+    """This tests is the balance calculated is correct"""
+    with client:
+        register_response = client.post("/register", data={
+            "email": "testuser123@test.com",
+            "password": "test123!test",
+            "confirm": "test123!test"
+        },
+                                        follow_redirects=True)
+        login_response = client.post("/login", data={
+            "email": "testuser123@test.com",
+            "password": "test123!test"
+        },
+                                     follow_redirects=True)
 
+        assert login_response.status_code == 200
 
-def test_user_balance(application):
-    # log = logging.getLogger("userBalance")
-    balance = 0.0
-    with application.app_context():
-        # create a record
-        user = User('keith@webizly.com', 'testtest')
-        db.session.add(user)
-        user = User.query.filter_by(email='keith@webizly.com').first()
-        # asserting that the user retrieved is correct
-        assert user.email == 'keith@webizly.com'
-        # check balance before update
-        assert user.balance == 0
+        form_data = {
+            "file": open('testing_resources/transactions.csv', 'rb')
+        }
 
-        user.accounts = [Transaction(1,5000, "CREDIT"), Transaction(1,1000, "DEBIT")]
-        db.session.commit()
+        balance_before_transaction = User.query.get(current_user.id).balance
+        # balance before any transaction
+        assert balance_before_transaction == 0
+        # This makes a call to upload the csv of transactions which will be processed.
+        transaction_csv_upload_response = client.post(
+            "/transactions/upload",
+            data=form_data,
+            follow_redirects=True)
 
-        #check balance for credit transaction
-        user_account1 = Transaction.query.filter_by(amount=5000).first()
-
-        if user_account1.transaction_type == "CREDIT":
-            balance = balance + user_account1.amount
-        else:
-            balance = balance - user_account1.amount
-
-        user_account1.balance = balance
-
-        assert user_account1.balance == 5000
-
-        # check balance for credit transaction
-        user_account2 = Transaction.query.filter_by(amount=1000).first()
-        if user_account2.transaction_type == "CREDIT":
-            balance = balance + user_account2.amount
-        else:
-            balance = balance - user_account2.amount
-
-        user_account2.balance = balance
-
-        assert user_account2.balance == 4000
+        assert transaction_csv_upload_response.status_code == 200
+        # balance after transaction
+        balance_after_transaction = User.query.get(current_user.id).balance
+        assert balance_after_transaction == 10601
